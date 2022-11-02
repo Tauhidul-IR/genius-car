99e113d570b389a41f4a0d6d8e52a78ab6a9bfb5
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-02 16:42:48 +0600

    2nd

--- src/Pages/Orders/OrderRow.js
+++ src/Pages/Orders/OrderRow.js
@@ -1,39 +1,39 @@
-// import React from 'react';
+import React from 'react';
 
-// const OrderRow = ({ order }) => {
-//     console.log(order)
-//     const { serviceName, price, email, phone, customer } = order;
-//     return (
-//         <tr>
-//             <th>
-//                 <label>
-//                     <input type="checkbox" className="checkbox" />
-//                 </label>
-//             </th>
-//             <td>
-//                 <div className="flex items-center space-x-3">
-//                     <div className="avatar">
-//                         <div className="mask mask-squircle w-12 h-12">
-//                             <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-//                         </div>
-//                     </div>
-//                     <div>
-//                         <div className="font-bold">{customer}</div>
-//                         <div className="text-sm opacity-50">{phone}</div>
-//                     </div>
-//                 </div>
-//             </td>
-//             <td>
-//                 {serviceName}
-//                 <br />
-//                 <span className="badge badge-ghost badge-sm">${price}</span>
-//             </td>
-//             <td>Purple</td>
-//             <th>
-//                 <button className="btn btn-ghost btn-xs">details</button>
-//             </th>
-//         </tr>
-//     );
-// };
+const OrderRow = ({ order }) => {
+    console.log(order)
+    const { serviceName, price, email, phone, customer } = order;
+    return (
+        <tr>
+            <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{customer}</div>
+                        <div className="text-sm opacity-50">{phone}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {serviceName}
+                <br />
+                <span className="badge badge-ghost badge-sm">${price}</span>
+            </td>
+            <td>Purple</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    );
+};
 
-// export default OrderRow;
+export default OrderRow;
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -5,7 +5,7 @@ import OrderRow from './OrderRow';
 const Orders = () => {
     const { user } = useContext(AuthContext);
     const [orders, setOrders] = useState({});
-    console.log(user)
+    console.log(user.email)
 
 
 
@@ -17,7 +17,36 @@ const Orders = () => {
     }, [user?.email])
     return (
         <div>
-            <h2>{orders.length}</h2>
+            <h2>Total Order : {orders.length}</h2>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow key={order._id} order={order}></OrderRow>)
+                        }
+
+
+                    </tbody>
+
+
+
+                </table>
+            </div>
         </div>
     );
 };
